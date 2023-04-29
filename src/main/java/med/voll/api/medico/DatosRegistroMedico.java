d2616d7c8efd647091110b29d5481ac99bf1a43e
Juan Camilo Sanchez Echeverri <lx08752@innovaccion.mx>
2023-04-28 19:25:44 -0500

    Ejercicios del aula 02.Request POST

---
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+
+import med.voll.api.DatosDireccion;
+
+public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad,
+                                  DatosDireccion direccion) {
+
+}
