9faa927c534258488f778f0c634da27b2068be39
Juan Camilo Sanchez Echeverri <lx08752@innovaccion.mx>
2023-04-29 18:40:39 -0500

    Ejercicios del aula 03.Spring Data JPA donde se agregan nuevas dependencias, se realizan validaciones de los datos a ingresar en la BD, la base de datos se hace en Azure y se usa Flyway como herramienta de migracion de proyectos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
     @PostMapping
-    public void registarMedico(@RequestBody DatosRegistroMedico datosRegistroMedico){
-        System.out.println("El request llega correctamente");
-        System.out.println(datosRegistroMedico);
+    public void registarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        medicoRepository.save(new Medico(datosRegistroMedico));
+
 
     }
 }
--- src/main/java/med/voll/api/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/medico/DatosRegistroMedico.java
@@ -1,8 +1,25 @@
 package med.voll.api.medico;
 
-import med.voll.api.DatosDireccion;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.direccion.DatosDireccion;
 
-public record DatosRegistroMedico(String nombre, String email, String documento, Especialidad especialidad,
+public record DatosRegistroMedico(@NotBlank
+                                  String nombre,
+                                  @NotBlank @Email
+                                  String email,
+                                  @NotBlank
+                                  String telefono,
+                                  @NotBlank
+                                  @Pattern(regexp = "\\d{7,14}")
+                                  String documento,
+                                  @NotNull
+                                  Especialidad especialidad,
+                                  @NotNull
+                                  @Valid
                                   DatosDireccion direccion) {
 
 }
